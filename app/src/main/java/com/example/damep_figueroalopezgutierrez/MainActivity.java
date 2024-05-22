ecfab473010903c144cbc17af6bc9733613b1a2a
RAUL ANDERSSON LOPEZ PURIS <20a3110155@untels.edu.pe>
2024-05-22 11:21:39 -0500

    tercer commit

--- app/src/main/java/com/example/damep_figueroalopezgutierrez/MainActivity.java
+++ app/src/main/java/com/example/damep_figueroalopezgutierrez/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.damep_figueroalopezgutierrez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.button);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envió sus dato", Toast.LENGTH_SHORT).show());
     }
 }
